65b095c5c47fe705e326592b5a059f14c8e843d6
Dipayan Koner <dipkoner1@gmail.com>
2025-02-08 01:04:53 +0530

    AI-Powered Healthcare Assistant

---
+++ test.py
@@ -0,0 +1,8 @@
+import transformers
+import transformers
+print(transformers.__version__)
+import sys
+sys.path.append(r"c:\users\dipay\appdata\local\programs\python\python310\lib\site-packages")
+
+import transformers
+print(transformers.__version__)
